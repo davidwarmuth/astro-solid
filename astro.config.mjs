72dec6728c0d459150d3beb2a332722016ff45ec
InVindicus <david-warmuth@web.de>
2025-06-12 22:41:43 +0200

    Added Tailwind CSS to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import solid from '@astrojs/solid-js';
+import { defineConfig } from "astro/config";
+import solid from "@astrojs/solid-js";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable Solid to support Solid JSX components.
-	integrations: [solid()],
+  // Enable Solid to support Solid JSX components.
+  integrations: [solid()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
