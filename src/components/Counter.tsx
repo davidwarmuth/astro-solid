8a4b8120f655c3b509a3f673381d9657e5f3d4cb
InVindicus <david-warmuth@web.de>
2025-06-12 22:56:12 +0200

    replaced css with tw classes

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,19 +1,22 @@
-import { createSignal, type JSX } from 'solid-js';
-import './Counter.css';
+import { createSignal, type JSX } from "solid-js";
 
 export default function Counter(props: { children?: JSX.Element }) {
-	const [count, setCount] = createSignal(0);
-	const add = () => setCount(count() + 1);
-	const subtract = () => setCount(count() - 1);
+  const [count, setCount] = createSignal(0);
+  const add = () => setCount(count() + 1);
+  const subtract = () => setCount(count() - 1);
 
-	return (
-		<>
-			<div class="counter">
-				<button onClick={subtract}>-</button>
-				<pre>{count()}</pre>
-				<button onClick={add}>+</button>
-			</div>
-			<div class="counter-message">{props.children}</div>
-		</>
-	);
+  return (
+    <>
+      <div class="mt-12 grid grid-cols-3 place-items-center text-3xl">
+        <button onClick={subtract} class="btn">
+          -
+        </button>
+        <pre>{count()}</pre>
+        <button onClick={add} class="btn">
+          +
+        </button>
+      </div>
+      <div class="mt-8 text-center">{props.children}</div>
+    </>
+  );
 }
